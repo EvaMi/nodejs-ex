1e8419aa823cd91a31decff0eaeed9e0382abe19
Milova <milova_evg@mail.ru>
2016-10-09 04:38:26 +0300

    IndexandServer

--- server.js
+++ server.js
@@ -4,7 +4,32 @@ var express = require('express'),
     app     = express(),
     eps     = require('ejs'),
     morgan  = require('morgan');
-    
+    var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server, options);
+server.listen(PORT);
+ 
+app.use('/static', express.static(__dirname + '/static'));
+ 
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+//подписываемся на событие соединения нового клиента
+io.sockets.on('connection', function (client) {
+    //подписываемся на событие message от клиента
+    client.on('message', function (message) {
+        try {
+            //посылаем сообщение себе
+            client.emit('message', message);
+            //посылаем сообщение всем клиентам, кроме себя
+            client.broadcast.emit('message', message);
+        } catch (e) {
+            console.log(e);
+            client.disconnect();
+        }
+    });
+});
+
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
